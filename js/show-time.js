9ccbd1fb23bc0f7e70b4c89d400028cf2723ad06
cylname <121369173+cylname@users.noreply.github.com>
2023-01-13 08:54:59 +0800

    Draft

---
+++ js/show-time.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+import timeTable from "/images/time-table.pdf";
+
+function App() {
+  return (
+    <div className="App">
+      <iframe src={timeTable} />
+    </div>
+  );
+}
